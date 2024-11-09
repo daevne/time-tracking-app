f4f748bc5d759fd0a88407bd1dadfd9242ed1711
daevne <bocsoeszti@gmail.com>
2024-11-09 19:58:56 +0100

    refactor: move emitter setup to plugin format, add comments

--- src/main.js
+++ src/main.js
@@ -1,12 +1,23 @@
 import { createApp } from 'vue';
 import mitt from 'mitt';
 import App from './App.vue';
+
+// Importing styles
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
+// Creating the event emitter
 const emitter = mitt();
+
+// Creating the Vue app instance
 const app = createApp(App);
 
-app.config.globalProperties.emitter = emitter;
+// Using a plugin to provide the emitter globally
+app.use({
+    install(app) {
+        app.config.globalProperties.emitter = emitter;
+    }
+});
 
+// Mounting the app
 app.mount('#app');
